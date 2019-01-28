2d66fe08e8cb6b92bc5f77f996180e18e4ace816
CaillouxZinzin <pierre.romon@etu.utc.fr>
2019-01-28 12:19:00 +0100

    lol

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Search from './Components/Search'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Search/>
+    );
+  }
+}
