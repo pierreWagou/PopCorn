1c6f80dc8f44eba20f2e9bdb85fb54fbf31586bb
CaillouxZinzin <pierre.romon@etu.utc.fr>
2019-01-28 12:19:08 +0100

    a fond

---
+++ Components/FilmItem.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {StyleSheet, View, Text, Image} from 'react-native'
+
+class FilmItem extends React.Component {
+  render() {
+    const film = this.props.film
+    return(
+      <View style={styles.container}>
+        <Image style={styles.image} source={{uri: "image"}}/>
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.titre}>
+              {film.title}
+            </Text>
+            <Text style={styles.vote}>
+              {film.vote_average}
+            </Text>
+          </View>
+          <Text style={styles.description} numberOfLines={6}>
+            {film.overview}
+          </Text>
+          <Text style={styles.sortie}>
+            Sorti le {film.release_date}
+          </Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 190,
+    flexDirection: 'row'
+  },
+  image: {
+    backgroundColor: 'gray',
+    margin: 5,
+    flex: 1
+  },
+  content: {
+    margin: 5,
+    flexDirection: 'column',
+    flex: 2
+  },
+  header: {
+    flex: 3,
+    flexDirection: 'row'
+  },
+  titre: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    flexWrap: 'wrap',
+    flex: 1
+  },
+  vote: {
+    paddingRight: 5,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontStyle: 'italic',
+    flex: 7
+  },
+  sortie: {
+    textAlign: 'right',
+    flex: 1
+  }
+})
+
+export default FilmItem
---
+++ Components/Search.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {View, TextInput, Button, StyleSheet, FlatList, Text} from 'react-native'
+import films from '../Helpers/FilmsData'
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBAPI'
+
+class Search extends React.Component {
+  _loadFilms(){
+    getFilmsFromApiWithSearchedText("star").then(data => console.log(data))
+  }
+  render() {
+    return(
+      <View style={styles.container}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <Button style={styles.button} title='Rechercher' onPress={()=>this._loadFilms()}/>
+        <FlatList
+          data={films}
+          keyExtractor={(item)=>item.id.toString}
+          renderItem={({item})=><FilmItem film={item}/>}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop:50
+  },
+  textinput: {
+    marginLeft:5,
+    marginRight:5,
+    height:50,
+    borderColor:'#000000',
+    borderWidth:2,
+    paddingLeft:5
+  },
+  button: {
+    height: 50
+  }
+})
+
+export default Search
